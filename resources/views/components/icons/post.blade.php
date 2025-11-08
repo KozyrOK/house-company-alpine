36f3f019ec6bbdf0766df36ba786577b7922e309
artem-kozyr <artem.kozyr777@gmail.com>
2025-11-08 20:28:18 +0200

    add icons

---
+++ resources/views/components/icons/post.blade.php
@@ -0,0 +1,4 @@
+<svg {{ $attributes->merge(['class' => 'w-12 h-12']) }} xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+    <path fill-rule="evenodd" d="M7 2.5a2.5 2.5 0 0 0-2.5 2.5v14a2.5 2.5 0 0 0 2.5 2.5h10a2.5 2.5 0 0 0 2.5-2.5V9.75a.75.75 0 0 0-.22-.53l-5.25-5.25a.75.75 0 0 0-.53-.22H7Zm1 7a.75.75 0 0 0 0 1.5h5a.75.75 0 0 0 0-1.5H8Zm0 3a.75.75 0 0 0 0 1.5h8a.75.75 0 0 0 0-1.5H8Zm0 3a.75.75 0 0 0 0 1.5h8a.75.75 0 0 0 0-1.5H8Z" clip-rule="evenodd"/>
+    <path d="M13.5 2v6.25c0 .414.336.75.75.75h4.25L13.5 2Z"/>
+</svg>
---
+++ resources/views/components/icons/user.blade.php
@@ -0,0 +1,4 @@
+<svg {{ $attributes->merge(['class' => 'w-12 h-12']) }} xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+    <path d="M12 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11Z"/>
+    <path d="M12 13c-4.47 0-8.5 3.58-8.5 8.5a1 1 0 1 0 2 0c0-3.77 3.13-6.5 6.5-6.5s6.5 2.73 6.5 6.5a1 1 0 1 0 2 0c0-4.92-4.03-8.5-8.5-8.5Z"/>
+</svg>
