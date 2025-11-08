a1ad98ee4d883c2713a99df69e75d3a059ba0d48
artem-kozyr <artem.kozyr777@gmail.com>
2025-11-08 02:07:35 +0200

    fix admin page

--- resources/views/pages/superadmin.blade.php
+++ resources/views/pages/superadmin.blade.php
@@ -6,26 +6,34 @@
     <section>
         <h1>{{ __('app.pages.admin_panel') }}</h1>
 
-        <div class="content-items-box-wrapper">
+        <div class="content-box-wrapper">
 
             <a href="{{ route('admin.users.index') }}"
                class="content-items-box">
-                <h2>Users</h2>
-                <p>Manage registered users and roles.</p>
+                <div class="content-item-box-wrapper">
+                    <x-icons.user/>
+                    <h2>Users</h2>
+                    <p>Manage registered users and roles</p>
+                </div>
             </a>
 
             <a href="{{ route('admin.companies.index') }}"
                class="content-items-box">
-                <h2>Companies</h2>
-                <p>View and edit company data.</p>
+                <div class="content-item-box-wrapper">
+                    <x-icons.company/>
+                    <h2>Companies</h2>
+                    <p>View and edit company data</p>
+                </div>
             </a>
 
             <a href="{{ route('admin.posts.index') }}"
                class="content-items-box">
-                <h2>Posts</h2>
-                <p>Moderate and approve company posts.</p>
+                <div class="content-item-box-wrapper">
+                    <x-icons.post/>
+                    <h2>Posts</h2>
+                    <p>Moderate and approve company posts</p>
+                </div>
             </a>
-
         </div>
     </section>
 @endsection
