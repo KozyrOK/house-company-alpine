a3c2048388596600d7d4f889672cdc23351838df
artem-kozyr <artem.kozyr777@gmail.com>
2026-06-13 03:42:57 +0300

    fix code on admin/users/show.blade.php

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -50,23 +50,17 @@
                         @endcan
                     @endif
 
-                    @can('update', $user)
-                            @if(auth()->user()->isSuperAdmin())
-                                <x-link text="app.buttons.users_companies" href="{{ route('admin.users.companies', $user) }}" class="button-list"/>
-                            @elseif(currentCompany())
-                                @can('excludeFromCompany', [$user, currentCompany()])
-                                    <form method="POST" action="{{ route('admin.users.companies.exclusion', [$user, currentCompany()]) }}" class="confirmable-form" data-confirm-message="{{ __('app.confirm.exclusion_user') }}">
-                                        @csrf
-                                        @method('DELETE')
-                                        <x-button text="app.buttons.exclusion_user" type="submit" class="button-delete"/>
-                                    </form>
-                                @endcan
-                            @endif
-                    @endcan
-                </div>
-
-                <div class="button-wrapper">
-                    <x-link text="app.buttons.users_companies" href="{{ route('admin.users.companies', $user) }}" class="button-list"/>
+                        @if(auth()->user()->isSuperAdmin())
+                            <x-link text="app.buttons.users_companies" href="{{ route('admin.users.companies', $user) }}" class="button-list"/>
+                        @elseif(currentCompany())
+                            @can('excludeFromCompany', [$user, currentCompany()])
+                                <form method="POST" action="{{ route('admin.users.companies.exclusion', [$user, currentCompany()]) }}" class="confirmable-form" data-confirm-message="{{ __('app.confirm.exclusion_user') }}">
+                                    @csrf
+                                    @method('DELETE')
+                                    <x-button text="app.buttons.exclusion_user" type="submit" class="button-delete"/>
+                                </form>
+                            @endcan
+                        @endif
                 </div>
 
                 <div class="button-wrapper">
